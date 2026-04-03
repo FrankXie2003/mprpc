f659a292c8d8d8bf9e6687824dbfd2efc8d9b404
Franky <1532481233@qq.com>
2026-04-04 01:05:58 +0800

    feat:

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "mprpcconfig.h"
+
 // mprpc框架的基础类.负责框架的一些初始化操作
 class MprpcApplication
 {
@@ -7,6 +9,9 @@ public:
     static void Init(int argc,char** argv);
     static MprpcApplication& GetInstance();
 private:
+    //普通成员变量不能在静态方法中
+    inline static MprpcConfig m_config;
+
     MprpcApplication();
     MprpcApplication(const MprpcApplication&) = delete;
     MprpcApplication(MprpcApplication&&) = delete;
